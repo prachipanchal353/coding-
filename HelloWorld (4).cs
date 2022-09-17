062ade33165b673a13c301e1785b38e889525392
prachipanchal353 <101180023+prachipanchal353@users.noreply.github.com>
2022-09-17 21:55:15 +0530

    Add files via upload

---
+++ HelloWorld (4).cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace PermutationsOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintResult(
+                Permute(new int[] { 1, 2 ,3 })
+            );
+        }
+
+        static IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list)
+        {
+            if (start == end)
+            {
+                
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1,end,  list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void PrintResult(IList<IList<int>> lists)
+        {
+           
+            foreach (var list in lists)
+            {
+                Console.WriteLine(   string.Join( ',',list));
+            }
+           
+        }
+    }
+}
+  
